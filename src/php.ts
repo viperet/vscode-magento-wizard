2aa10efa3b0e28fda66cc5491510dc33dfcea6a0
Vitaly Peretiatko <viperet@gmail.com>
2019-11-10 20:12:36 +0200

    Added better class name / namespace validation

--- src/php.ts
+++ src/php.ts
@@ -196,3 +196,9 @@ class Php {
 }
 
 export default Php;
+
+export const reservedWords: string[] = ['__halt_compiler', 'abstract', 'and', 'array', 'as', 'break', 'callable', 'case', 'catch', 'class', 'clone', 'const', 'continue', 'declare', 'default', 'die',
+'do', 'echo', 'else', 'elseif', 'empty', 'enddeclare', 'endfor', 'endforeach', 'endif', 'endswitch', 'endwhile', 'eval', 'exit', 'extends', 'final', 'for', 'foreach', 'function', 'global',
+'goto', 'if', 'implements', 'include', 'include_once', 'instanceof', 'insteadof', 'interface', 'isset', 'list', 'namespace', 'new', 'or', 'print', 'private', 'protected', 'public', 'require',
+'require_once', 'return', 'static', 'switch', 'throw', 'trait', 'try', 'unset', 'use', 'var', 'while', 'xor', 'int', 'float', 'bool', 'string', 'true', 'false', 'null', 'void', 'iterable',
+'object', 'resource', 'mixed', 'numeric'];
