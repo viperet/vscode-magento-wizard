1c518b0b71f6a067a7469ef687a64f5f07f233c0
Vitaly Peretiatko <viperet@gmail.com>
2021-05-13 14:36:42 +0300

    Updated requirejs-config.js template

--- templates/requirejs-config.js
+++ templates/requirejs-config.js
@@ -2,6 +2,13 @@ var config = {
     map: {
         '*': {
             '${1:scriptName}':'{{vendor}}_{{extension}}/js/scriptname'
+        }
+    },
+    config: {
+        mixins: {
+            'Magento_Checkout/js/model/shipping-save-processor/payload-extender': {
+                '{{vendor}}_{{extension}}/js/model/shipping-save-processor/payload-extender-mixin': true
             }
         }
-    };
+    }
+};
