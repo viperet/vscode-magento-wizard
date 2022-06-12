cc31a443594aca3791bc61bec6c563f91ae8edd4
Vitaly Peretiatko <viperet@gmail.com>
2022-06-13 00:26:41 +0300

    Templates improvements and bugfixes

--- src/templates.ts
+++ src/templates.ts
@@ -27,11 +27,12 @@ const templates: { [key: string]: string } = {
     '\/Model\/.*\.php$': require('../templates/model.php'),
     '\/Observer\/.*\.php$': require('../templates/observer.php'),
     '\/Console\/.*\.php$': require('../templates/command.php'),
-    '\/Setup\/InstallData.php$': require('../templates/Setup/InstallData.php'),
-    '\/Setup\/InstallSchema.php$': require('../templates/Setup/InstallSchema.php'),
-    '\/Setup\/Recurring.php$': require('../templates/Setup/Recurring.php'),
-    '\/Setup\/UpgradeData.php$': require('../templates/Setup/UpgradeData.php'),
-    '\/Setup\/UpgradeSchema.php$': require('../templates/Setup/UpgradeSchema.php'),
+    '\/Setup\/InstallData\.php$': require('../templates/Setup/InstallData.php'),
+    '\/Setup\/InstallSchema\.php$': require('../templates/Setup/InstallSchema.php'),
+    '\/Setup\/Recurring\.php$': require('../templates/Setup/Recurring.php'),
+    '\/Setup\/UpgradeData\.php$': require('../templates/Setup/UpgradeData.php'),
+    '\/Setup\/UpgradeSchema\.php$': require('../templates/Setup/UpgradeSchema.php'),
+    'Interface\.php$': require('../templates/interface.php'),
     '.php$': require('../templates/class.php'),
 };
 
---
+++ templates/interface.php
@@ -0,0 +1,14 @@
+<?php
+namespace {{ namespace }};
+
+interface {{ name }}
+{
+    /**
+     * Undocumented function
+     *
+     * @param [type] $param
+     * @return string
+     */
+    public function methodName($param);
+
+}
